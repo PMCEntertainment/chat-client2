56233a448b0013cf7a8a4ceb6fb6154f95db2d3b
Phương Mỹ Chi <developer@phuongmychi.vn>
2023-06-08 07:44:22 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,92 @@
-import logo from './logo.svg';
-import './App.css';
+
+import React, {
+useState,
+    useEffect
+} from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [messages, setMessages] = useState([]);
+    const [inputValue, setInputValue] = useState('');
+
+    useEffect(() => {
+        fetchMessages();
+    }, []);
+
+    const fetchMessages = async () => {
+        try {
+            const response = await fetch('http://localhost:8080/messages');
+            const data = await response.json();
+            setMessages(data);
+        } catch (error) {
+            console.error('Error fetching messages:', error);
+        }
+    };
+
+    const handleInputChange = event => {
+        setInputValue(event.target.value);
+    };
+
+    const handleSendMessage = async event => {
+        event.preventDefault();
+
+        if (inputValue.trim() !== '') {
+            const message = {
+                content: inputValue,
+                sender: 'User1', // Thay đổi thành người dùng hiện tại nếu cần thiết
+            };
+
+            try {
+                await fetch('http://localhost:8080/messages', {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                    body: JSON.stringify(message),
+                });
+            } catch (error) {
+                console.error('Error sending message:', error);
+            }
+
+            setInputValue('');
+        }
+    };
+
+    return ( <
+            div >
+            <
+            ul > {
+                messages.map((message, index) => ( <
+                        li key = {
+                            index
+                        } >
+                        <
+                        strong > {
+                            message.sender
+                        }: < /strong> {
+                        message.content
+                    } <
+                    /li>
+                ))
+        } <
+        /ul> <
+    form onSubmit = {
+            handleSendMessage
+        } >
+        <
+        input type = "text"
+    value = {
+        inputValue
+    }
+    onChange = {
+        handleInputChange
+    }
+    /> <
+    button type = "submit" > Send < /button> < /
+        form > <
+        /div>
+);
 }
 
-export default App;
+export default App
+
+
